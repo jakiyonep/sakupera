5c5ac7f8c9e83a817044cc83670650d573d9a893
jakiyonep <akiyone0820@gmail.com>
2024-03-11 00:07:36 +0900

    Modified wsgi settings from local to prod

--- SakuperaSite/SakuperaSite/settings/prod.py
+++ SakuperaSite/SakuperaSite/settings/prod.py
@@ -1,21 +1,20 @@
 from .base import *
-import environ
 
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
-SECRET_KEY = env.get_value.get_value('SECRET_KEY')
+SECRET_KEY = env.get_value('SECRET_KEY')
 
 DATABASES = {
     'default': {
-        'ENGINE': env.get_value('DB_ENGINE'),
-        'NAME': env.get_value('DB_NAME'),
-        'USER': env.get_value('DB_USER'),
-        'PASSWORD': env.get_value('DB_PW'),
-        'HOST': env.get_value('DB_HOST'),
-        'PORT': env.get_value('DB_PORT'),
+        'ENGINE': env('DB_ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PW'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
