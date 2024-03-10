7410c97a587827bacd868b6b5459339c1ba03a71
jakiyonep <akiyone0820@gmail.com>
2024-03-10 23:45:52 +0900

    Modified wsgi settings from local to prod

--- SakuperaSite/SakuperaSite/settings/prod.py
+++ SakuperaSite/SakuperaSite/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['www.sakupera.com']
 
 SECRET_KEY = env('SECRET_KEY')
